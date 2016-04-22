f8d7ddec846e0a58d6fffbec5793fa45c18826c5
Jan <jasiu@360.st>
2016-04-22 22:11:14 +0200

    add card resizing dynamically

---
+++ src/KeepItSolved/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿//site.js
+if ($(window).width() < 768) {
+	$("#newCard").css("width", "100%");
+	$("#questionLabel").css("color", "white");
+}
+else {
+	$("#newCard").css("width", "625px");
+}
+$(window).resize(function () {
+	if ($(window).width() < 768) {
+		$("#newCard").css("width", "100%");
+		$("#questionLabel").css("color", "white");
+	}
+	else {
+		$("#newCard").css("width", "625px");
+		$("#questionLabel").css("color", "#2c3e50");
+	}
+});
