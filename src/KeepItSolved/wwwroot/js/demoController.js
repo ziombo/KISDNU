a4cb914e29aae18677a49080d9d30047ed4cdde5
Jan <jasiu@360.st>
2016-04-26 15:58:53 +0200

    updated demo

--- src/KeepItSolved/wwwroot/js/demoController.js
+++ src/KeepItSolved/wwwroot/js/demoController.js
@@ -18,22 +18,10 @@
 			answer: "http://http://www.flashscore.com/",
 			category: "Sports"
 		}, {
-			question: "Where can I find football live scores?",
-			answer: "http://http://www.flashscore.com/",
-			category: "Sports"
-		}, {
-			question: "Where can I find football live scores?",
-			answer: "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker",
-			category: "Sports"
-		}, {
-			question: "Where can I find football live scores?",
-			answer: "http://http://www.flashscore.com/",
-			category: "Sports"
-		}, {
-			question: "Where can I find football live scores?",
-			answer: "http://http://www.flashscore.com/",
-			category: "Sports"
-		},
+			question: "What if there's alot of text?",
+			answer: "Thats where scrollbar comes with help. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.",
+			category: "Site showcase"
+		}
 		];
 
 		vm.newCard = {};
